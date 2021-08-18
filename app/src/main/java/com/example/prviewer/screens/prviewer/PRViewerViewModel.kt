9de29f0026812e0b877d15f95e50380a949c7c31
kishan kumar <kishan@kishans-MacBook-Air.local>
2021-08-18 22:28:32 +0530

    progress bar added

--- app/src/main/java/com/example/prviewer/screens/prviewer/PRViewerViewModel.kt
+++ app/src/main/java/com/example/prviewer/screens/prviewer/PRViewerViewModel.kt
@@ -9,22 +9,32 @@ import com.example.prviewer.repository.PRRepository
 import kotlinx.coroutines.launch
 
 class PRViewerViewModel(
-    private val prRepository: PRRepository
-) : ViewModel() {
+    private val prRepository: PRRepository)
+    : ViewModel() {
 
     init {
-        viewModelScope.launch {
-            getPullRequest()
-        }
+        viewModelScope.launch { getPullRequest() }
     }
 
     private val prMutableLiveData = MutableLiveData<List<PRModel>>();
-
+    private lateinit var isLoadingLiveData: MutableLiveData<Boolean>
     val prLiveData : LiveData<List<PRModel>>
         get() = prMutableLiveData
+
+
+    fun isLoading(): LiveData<Boolean> {
+        if (!::isLoadingLiveData.isInitialized) {
+            isLoadingLiveData = MutableLiveData()
+            isLoadingLiveData.value = true
+        }
+        return isLoadingLiveData
+    }
+
+
 
     suspend fun getPullRequest(){
         val pullRequests = prRepository.getPullRequests();
         prMutableLiveData.value = pullRequests!!
+        isLoadingLiveData.value=false
     }
 }
