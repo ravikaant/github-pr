be8af6d4d57cf712ff6c8e87f4ea1546280416f5
kishan kumar <kishan@kishans-MacBook-Air.local>
2021-08-19 07:34:07 +0530

    username and reponame added to constant

--- app/src/main/java/com/example/prviewer/utils/Constant.kt
+++ app/src/main/java/com/example/prviewer/utils/Constant.kt
@@ -1,8 +1,13 @@
 package com.example.prviewer.utils
 
 object constant {
+
+    val USERNAME  = "git-kishan"
+    val REPONAME  = "PR-viewer"
     val BASE_URL = "https://api.github.com/";
+
     val CLOSED = "closed"
     val OPEN = "open"
+    val ALL = "all"
 
 }
